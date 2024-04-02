054af3406dc3faa586f7550d813fe70b80dc4792
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-04-02 19:15:09 -0400

    improved project file structure

--- models/note.js
+++ models/note.js
@@ -1,32 +1,14 @@
-const mongoose = require("mongoose"); // Importing the mongoose module
+const mongoose = require("mongoose");
 
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
-
-// Connecting to the MongoDB database
-mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
-
-// Defining the schema for the Note model
 const noteSchema = new mongoose.Schema({
   content: {
-    // Define specific validation rules for the content field
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5,
   },
   important: Boolean,
 });
 
-// Modifying the toJSON method of the schema to format the returned object
 noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
@@ -35,5 +17,4 @@ noteSchema.set("toJSON", {
   },
 });
 
-// Exporting the Note model
 module.exports = mongoose.model("Note", noteSchema);
