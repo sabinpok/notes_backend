d03181455fa16929f318010b30573d33a993f7a3
Sabin Pokhrel <118865601+sabinpok@users.noreply.github.com>
2024-05-13 16:41:26 -0400

    refactored route to fetch notes using async/await

--- controllers/notes.js
+++ controllers/notes.js
@@ -10,22 +10,21 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
 })
 
 notesRouter.get('/:id', (request, response, next) => {
   Note.findById(request.params.id)
-    .then((note) => {
+    .then(note => {
       if (note) {
         response.json(note)
       } else {
         response.status(404).end()
       }
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 notesRouter.post('/', (request, response, next) => {
@@ -33,15 +32,15 @@ notesRouter.post('/', (request, response, next) => {
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important || false
   })
 
   note
     .save()
-    .then((savedNote) => {
+    .then(savedNote => {
       response.json(savedNote)
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 notesRouter.delete('/:id', (request, response, next) => {
@@ -49,7 +48,7 @@ notesRouter.delete('/:id', (request, response, next) => {
     .then(() => {
       response.status(204).end()
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -57,14 +56,14 @@ notesRouter.put('/:id', (request, response, next) => {
 
   const note = {
     content: body.content,
-    important: body.important,
+    important: body.important
   }
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
+    .then(updatedNote => {
       response.json(updatedNote)
     })
-    .catch((error) => next(error))
+    .catch(error => next(error))
 })
 
 /*
